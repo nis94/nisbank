6b531f7ecd49a68b671b4d80fbfbbc65ea268a49
nirbarlevav <nir.barlevav@shieldfc.com>
2024-06-08 23:50:30 +0300

    - Renaming - Local Config (no webhook yet) - running with local rabbit

---
+++ accounts/src/main/java/com/nisbank/accounts/AccountsApplication.java
@@ -0,0 +1,43 @@
+package com.nisbank.accounts;
+
+import com.nisbank.accounts.dto.AccountContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {AccountContactInfoDto.class})
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "NisBank Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Nir Bar Levav",
+						email = "nir@nisbank.com",
+						url = "https://www.nisbank94.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.nisbank94.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "NisBank Accounts microservice REST API Documentation",
+				url = "https://www.nisbank94.com/swagger-ui.html"
+		)
+)
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
---
+++ accounts/src/main/java/com/nisbank/accounts/mapper/AccountsMapper.java
@@ -0,0 +1,22 @@
+package com.nisbank.accounts.mapper;
+
+import com.nisbank.accounts.dto.AccountDto;
+import com.nisbank.accounts.entity.Account;
+
+public class AccountsMapper {
+
+    public static AccountDto mapToAccountsDto(Account account, AccountDto accountDto) {
+        accountDto.setAccountNumber(account.getAccountNumber());
+        accountDto.setAccountType(account.getAccountType());
+        accountDto.setBranchAddress(account.getBranchAddress());
+        return accountDto;
+    }
+
+    public static Account mapToAccounts(AccountDto accountDto, Account account) {
+        account.setAccountNumber(accountDto.getAccountNumber());
+        account.setAccountType(accountDto.getAccountType());
+        account.setBranchAddress(accountDto.getBranchAddress());
+        return account;
+    }
+
+}
